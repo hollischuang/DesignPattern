3587c489d6da639fab16083eb83918114d6f2ba1
hollis.zhl <hollis.zhl@alibaba-inc.com>
2016-04-26 19:14:23 +0800

    修改构造方法的声明

--- src/main/java/com/hollischuang/pattern/singlenton/Enum/Singleton.java
+++ src/main/java/com/hollischuang/pattern/singlenton/Enum/Singleton.java
@@ -3,9 +3,10 @@ package com.hollischuang.pattern.Singlenton.Enum;
 /**
  * Created by hollis on 16/4/6.
  */
-public enum  Singleton {
+public enum Singleton {
 
     INSTANCE;
-    public void Singleton() {
+
+    Singleton() {
     }
 }
